dab0e7cecff407e8916721c7dc2a6f2b27464ca1
gauthamej <52912918+gauthamej@users.noreply.github.com>
2019-07-16 22:15:42 -0700

    Create vowelnot.py

---
+++ vowelnot.py
@@ -0,0 +1,9 @@
+ch=input()
+CH=ch.upper()
+if(CH=='A' or CH=='E' or CH=='I' or CH=='O' or CH=='U'):
+    print("vowel")
+elif(CH=='B'or CH=='C' or CH=='D'or CH=='F' or CH=='G'or CH=='H'or CH=='J'or CH=='K'or CH=='L' or CH=='M'or CH=='N'or CH=='P'or CH=='Q'or CH=='R'or CH=='S'or CH=='T'or CH=='V'or CH=='W'or CH=='X'or CH=='Y'or CH=='Z'):
+    print("constant")
+else:
+    print("invalid")
+    
